4aac21f4d896192639337c327eae8deaeee9ab56
Thayne Trevenen <devops@opensailsdev.com>
2013-07-09 17:11:23 -0600

    vl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qrcode::Application.config.secret_key_base = '4a33ebed0befc6dc20753d0e4b4796f1cea1fba0e5cbeb4c794f109940f87e46cf18f052290b007fd6156525b830da0d5ab11a9f508087580e4972ceb2ba4b13'
